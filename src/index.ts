ff188309e87138f9eacc2fbb7d3b7653100d97a3
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-13 09:35:07 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+const app = new Hono();
+const adapter = new PrismaLibSQL({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+const prisma = new PrismaClient({ adapter });
+
+app.get("/", async (c) => {
+  const user = await prisma.user.findMany();
+  console.log(user);
+  return c.json(user);
+});
+app.post("/", async (c) => {
+  const { email, name, password } = await c.req.json();
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password,
+    },
+  });
+  return c.json(newUser);
+});
+
+export default app;
