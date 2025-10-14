20347faa41d7a108b8c6a3e1790199a11462f7ef
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-14 14:39:57 +0530

    change7

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { cors } from "hono/cors";
+import { auth } from "./lib/auth";
 
 const app = new Hono();
 const adapter = new PrismaLibSQL({
@@ -10,30 +11,39 @@ const adapter = new PrismaLibSQL({
 });
 const prisma = new PrismaClient({ adapter });
 
+// app.use(
+//   "*",
+//   cors({
+//     origin: "*",
+//     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
 app.use(
-  "*",
+  "/api/*",
   cors({
-    origin: "*",
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowHeaders: ["Content-Type", "Authorization"],
+    origin: "http://localhost:5173", // Replace with your frontend's URL
     credentials: true,
   })
 );
+
+app.use("/api/auth/*", (c) => auth.handler(c.req.raw));
 
 app.get("/", async (c) => {
   const user = await prisma.user.findMany();
   return c.json(user);
 });
-app.post("/", async (c) => {
-  const { email, name, password } = await c.req.json();
-  const newUser = await prisma.user.create({
-    data: {
-      email,
-      name,
-      password,
-    },
-  });
-  return c.json(newUser);
-});
+// app.post("/", async (c) => {
+//   const { email, name, password } = await c.req.json();
+//   const newUser = await prisma.user.create({
+//     data: {
+//       email,
+//       name,
+//       password,
+//     },
+//   });
+//   return c.json(newUser);
+// });
 
 export default app;
---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+const adapter = new PrismaLibSQL({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+const prisma = new PrismaClient({ adapter });
+export const auth = betterAuth({
+  emailAndPassword: { enabled: true },
+  secret: process.env.BETTER_AUTH_SECRET || "",
+  baseUrl: process.env.BETTER_AUTH_URL || "",
+  database: prismaAdapter(prisma, { provider: "sqlite" }),
+  trustedOrigins: ["http://localhost:5173", "http://localhost:3000"],
+});
