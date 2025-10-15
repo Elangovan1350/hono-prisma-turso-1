a0af0f8c3665b20480f80e0736d379d148407a51
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-15 12:34:50 +0530

    change20

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,23 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { cors } from "hono/cors";
-// import { auth } from "./auth";
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
+import { auth } from "./auth.js";
 
 const app = new Hono();
 const adapter = new PrismaLibSQL({
   url: `${process.env.TURSO_DATABASE_URL}`,
   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 });
-// const prisma = new PrismaClient({ adapter });
-
-// const adapter = new PrismaLibSQL({
-//   url: `${process.env.TURSO_DATABASE_URL}`,
-//   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-// });
 const prisma = new PrismaClient({ adapter });
-const auth = betterAuth({
-  emailAndPassword: { enabled: true },
-  database: prismaAdapter(prisma, { provider: "sqlite" }),
-  trustedOrigins: [
-    "http://localhost:5173",
-    "https://react-js-froendend.vercel.app/",
-  ],
-});
 
 app.use(
   "*",
   cors({
     origin: ["http://localhost:5173", "https://react-js-froendend.vercel.app"],
-    // allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    // allowHeaders: ["Content-Type", "Authorization"],
+
     credentials: true,
   })
 );
-// app.use(
-//   "/api/*",
-//   cors({
-//     origin: "http://localhost:5173",
-
-//     credentials: true,
-//   })
-// );
 
 app.use("/api/auth/*", (c) => auth.handler(c.req.raw));
 app.get("/", (c) => c.text("Hello Hono!"));
