b33b40873964e1d87c6f4c7f2c8decf85c29cbbd
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-15 08:35:48 +0530

    change14

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { PrismaClient } from "./prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { cors } from "hono/cors";
 import { auth } from "./lib/auth";
