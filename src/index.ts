a8e4230a26321ddacfe778bb3414772bc6a5038d
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-15 20:16:55 +0530

    change23

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { cors } from "hono/cors";
 import { auth } from "./utils/auth.js";
+import { prisma } from "./utils/prismaConnect.js";
 
 const app = new Hono();
-const adapter = new PrismaLibSQL({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-});
-const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaLibSQL({
+//   url: `${process.env.TURSO_DATABASE_URL}`,
+//   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+// });
+// const prisma = new PrismaClient({ adapter });
 
 app.use(
   "*",
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,13 +1,14 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { prisma } from "./prismaConnect.js";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
-const adapter = new PrismaLibSQL({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-});
-const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaLibSQL({
+//   url: `${process.env.TURSO_DATABASE_URL}`,
+//   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+// });
+// const prisma = new PrismaClient({ adapter });
 export const auth = betterAuth({
   emailAndPassword: { enabled: true },
   database: prismaAdapter(prisma, { provider: "sqlite" }),
