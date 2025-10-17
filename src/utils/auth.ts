0e39a496953ac7c40bc43d4e67b48e90d634aede
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-17 14:44:55 +0530

    change32

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -13,4 +13,25 @@ export const auth = betterAuth({
   emailAndPassword: { enabled: true },
   database: prismaAdapter(prisma, { provider: "sqlite" }),
   trustedOrigins: [`${process.env.HOST_URL}`],
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: true,
+      domain: process.env.HOST_URL,
+      // domain: process.env.COOKIE_DOMAIN || undefined,
+    },
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HOST_URL,
+
+      // domain: process.env.COOKIE_DOMAIN || undefined,
+      path: "/",
+    },
+    defaultCookieAttributes: {
+      secure: true,
+      // httpOnly: true,
+      sameSite: "none", // Allows CORS-based cookie sharing across subdomains
+      // partitioned: true, // New browser standards will mandate this for foreign cookies
+    },
+  },
 });
